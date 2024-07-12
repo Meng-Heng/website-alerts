a98157a796e45a1530db29c14f5e90ea2243e24a
Meng-Heng <havmengheng111@gmail.com>
2024-07-12 14:36:14 +0700

    initial

---
+++ tests/Feature/initialTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace mengheng\ParsedownAlerts\Tests;
+
+
+use Orchestra\Testbench\TestCase;
+use Parsedown;
+
+class InitialTest extends TestCase {
+
+    public function alerts_test() {
+       $this->assertTrue(true);
+    }
+}
+
+?>
---
+++ tests/test.md
@@ -0,0 +1,7 @@
+---
+title: mytitle
+---
+
+>[!important]
+>IMportant
+
